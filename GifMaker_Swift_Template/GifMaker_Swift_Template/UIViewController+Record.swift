9aa087b7bd563e7ce80cac633510bfa022a7a590
patrickmontalto <pmontalto@gmail.com>
2016-06-17 11:03:35 -0400

    enable simple editing

--- GifMaker_Swift_Template/GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/GifMaker_Swift_Template/UIViewController+Record.swift
@@ -13,23 +13,67 @@ import MobileCoreServices
 // Constants for Regift
 let frameCount = 16
 let delayTime: Float = 0.2
+let frameRate = 15
 let loopCount = 0 // 0 means infinite loop
 
 extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: Present Video Options
+    @IBAction func presentVideoOptions() {
+        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            self.launchPhotoLibrary()
+        } else {
+            // Make new alert controller - action sheet style
+            let newGifActionSheet = UIAlertController(title: "Create new GIF", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            // Make actions: recordVideo, chooseFromExisting, cancel
+            let recordVideo = UIAlertAction(title: "Record a Video", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                self.launchVideoCamera()
+            })
+            
+            let chooseFromExisting = UIAlertAction(title: "Choose from Existing", style: .Default, handler: { (alert) -> Void in
+                self.launchPhotoLibrary()
+            })
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            
+            newGifActionSheet.addAction(recordVideo)
+            newGifActionSheet.addAction(chooseFromExisting)
+            newGifActionSheet.addAction(cancel)
+            
+            presentViewController(newGifActionSheet, animated: true, completion: nil)
+            newGifActionSheet.view.tintColor = UIColor(red: 1.0, green: 65.0/255.0, blue: 112.0/255.0, alpha: 1.0)
+        }
+        
+    }
+    
+    // MARK: Present Photo Library
+    func launchPhotoLibrary() {
+        
+        let photoPickerController = imagePickerWithSource(UIImagePickerControllerSourceType.PhotoLibrary)
+        
+        // Present controller
+        presentViewController(photoPickerController, animated: true, completion: nil)
+    }
+    
     // MARK: Present Video Camera
-    @IBAction func launchVideoCamera(sender: AnyObject) {
+    func launchVideoCamera() {
         
-        // create imagePicker
-        let recordVideoControler = UIImagePickerController()
-        
-        // Set properties: sourceType, mediaTypes, allowsEditing, delegate
-        recordVideoControler.sourceType = UIImagePickerControllerSourceType.Camera
-        recordVideoControler.mediaTypes = [kUTTypeMovie as String]
-        recordVideoControler.allowsEditing = false
-        recordVideoControler.delegate = self
+        let recordVideoControler = imagePickerWithSource(UIImagePickerControllerSourceType.Camera)
         
         // Present controller
         presentViewController(recordVideoControler, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerController Creation
+    func imagePickerWithSource(source: UIImagePickerControllerSourceType) -> UIImagePickerController {
+        let pickerController = UIImagePickerController()
+        
+        pickerController.sourceType = source
+        pickerController.mediaTypes = [kUTTypeMovie as String]
+        pickerController.allowsEditing = true
+        pickerController.delegate = self
+        
+        return pickerController
     }
     
     // MARK: UIImagePickerController Delegate
@@ -41,15 +85,21 @@ extension UIViewController: UIImagePickerControllerDelegate, UINavigationControl
         if mediaType == kUTTypeMovie as String {
             let rawVideoURL = info[UIImagePickerControllerMediaURL] as! NSURL
             
+            // Get start time & duration
+            let startTime = info["_UIImagePickerControllerVideoEditingStart"] as? Float
+            let endTime = info["_UIImagePickerControllerVideoEditingEnd"] as? Float
+            var duration: Float?
+            
+            if let startTime = startTime, endTime = endTime {
+                duration = endTime - startTime
+
+            }
+            // TODO: Crop video to square
+            
             // Dismiss view controller
             dismissViewControllerAnimated(true, completion: nil)
             // Convert video to GIF
-            convertVideoToGIF(rawVideoURL)
-            //UISaveVideoAtPathToSavedPhotosAlbum(rawVideoURL.path!, nil, nil, nil)
-            
-            // TODO: Get start and end points for trimmed video
-            
-            // TODO: Crop video to square
+            convertVideoToGIF(rawVideoURL, start: startTime, duration: duration)
         }
         
         
@@ -60,8 +110,16 @@ extension UIViewController: UIImagePickerControllerDelegate, UINavigationControl
     }
     
     // MARK: - GIF Conversion Methods
-    func convertVideoToGIF(videoURL: NSURL) {
-        let regift = Regift(sourceFileURL: videoURL, destinationFileURL: nil, frameCount: frameCount, delayTime: delayTime, loopCount: loopCount)
+    func convertVideoToGIF(videoURL: NSURL, start: Float?, duration: Float?) {
+        var regift: Regift;
+        
+        if let start = start, duration = duration {
+            // Trimmed
+            regift = Regift(sourceFileURL: videoURL, destinationFileURL: nil, startTime: start, duration: duration, frameRate: frameRate, loopCount: loopCount)
+        } else {
+            regift = Regift(sourceFileURL: videoURL, destinationFileURL: nil, frameCount: frameCount, delayTime: delayTime, loopCount: loopCount)
+        }
+        
         let gifURL = regift.createGif()
         let gif = Gif(GifUrl: gifURL!, videoURL: videoURL, caption: nil)
         displayGIF(gif)
